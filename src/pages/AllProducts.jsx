9bce1d833e2a577f93e4b85a8d137fb454db97bf
katukuri_jaswanth <katukurijaswanth2@gmail.com>
2026-04-21 22:54:41 +0530

    after 404 error fix

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -3,19 +3,23 @@ import { useEffect, useState } from "react";
 
 import { ProductGrid } from "../features/products/components/ProductGrid.jsx";
 import { useNavigate } from "react-router-dom";
+import { ProductsSkeleton } from "../features/products/skeleton/ProductsSkeleton.jsx";
 
 export const AllProducts = () => {
   const [allproducts, setAllproducts] = useState([]);
   const navigate = useNavigate();
+   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get("https://dummyjson.com/products?limit=100")
       .then((res) => {
         setAllproducts(res.data.products);
+          setLoading(false);
       })
       .catch((err) => {
         console.log(err);
+          setLoading(false);
       });
   }, []);
 
@@ -25,8 +29,10 @@ export const AllProducts = () => {
   };
 
   return (
-    <>
+    <>{loading ? (
+      <ProductsSkeleton />
+    ):(
       <ProductGrid products={allproducts} onBuyNow={handleBuyNow} />
-    </>
+)}</>
   );
 };
